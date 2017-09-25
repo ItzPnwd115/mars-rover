715d9f9651bfdcbd5cbef83f2a04104d196c93a6
Sean Donlin <itzpnwd115@gmail.com>
2017-09-25 10:31:41 -0400

    LOL

--- Rover.java
+++ Rover.java
@@ -12,6 +12,7 @@ public class Rover
     private int x;
     private int y;
     private int dir; // 0=North, 1=East, 2=South, 3=West
+    private int numPics;
     
     
     // constructor(s)
@@ -21,32 +22,49 @@ public class Rover
         this.x = 0;
         this.y = 0;
         this.dir = 0;
+        this.numPics = 0;
     }
     
     
     // methods - stuff the Rover can do
-    public void move()
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public void takePic()
+    {
+        System.out.println(name + " took a picture at [" + x +"," + y + "] facing " + dir);
+    }
+    
+    public void move(int n)
     {
         if (dir == 0)
         {
-            y += 1;
+            y += n;
         }
         else if (dir == 1)
         {
-            x += 1;
+            x += n;
         }
         else if (dir == 2)
         {
-            y -= 1;
+            y -= n;
         }
         else 
         {
-            x -= 1;
+            x -= n;
         }
         
-        System.out.println(name + " moved in direction " + dir);
+        if(n == 1)
+        {
+            System.out.println(name + " moved in direction " + dir + " by " + n + " unit.");
+        }
+        else{
+            System.out.println(name + " moved in direction " + dir + " by " + n + " units.");
+        }
     }
-    
+        
     public void rotateLeft() 
     {
         dir -= 1;
--- RoverRunner.java
+++ RoverRunner.java
@@ -14,21 +14,26 @@ public class RoverRunner
         Rover r3 = new Rover("Lil-Boat");
         
         System.out.println(r1);
-        r1.move();
+        r1.move(1);
         r1.rotateRight();             
-        r1.move();
+        r1.move(1);
+        r1.setName("Sojourner");
         System.out.println(r1);
         
         System.out.println(r2);
         r2.rotateRight();
-        r2.move();        
-        r2.move();        
-        r2.move();       
-        r2.move();        
-        r2.move();       
+        r2.move(5);        
         r2.rotateRight();       
-        r2.move();        
-        r2.move();
+        r2.move(2);
+        r2.takePic();
         System.out.println(r2);
+        
+        System.out.println(r3);
+        r3.move(2);
+        r3.rotateLeft();
+        r3.move(1);
+        System.out.print(r3);
+        
+        
     }
 }
